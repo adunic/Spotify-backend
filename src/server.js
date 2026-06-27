0d2a3516923362940f337b9df661c2a4050567d7
adunic <dev72sharma@gmail.com>
2026-06-27 21:12:43 +0530

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const app = require('./app');
+require('dotenv').config();
+const connectDB = require('./db/db');
+
+connectDB();
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
